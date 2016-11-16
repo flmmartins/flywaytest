432923c8bc49e402b64249864f26b01018cecf18
Fernanda Martins <flmmartins@gmail.com>
2016-11-16 12:26:46 -0200

    Fix incorrect migration

--- sql/V3__Delete_people.sql
+++ sql/V3__Delete_people.sql
@@ -1,3 +1,1 @@
-delete from PERSON (ID, NAME) values (1, 'Axel');
-delete from PERSON (ID, NAME) values (2, 'Mr. Foo');
-delete from PERSON (ID, NAME) values (3, 'Ms. Bar');
+delete from PERSON where id=1;
